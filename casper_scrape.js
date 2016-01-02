a325b08fe5caab06ca3b1215038e0afafab737f1
Stanley Zheng <szheng@thisisgrow.com>
2016-01-02 11:34:30 -0500

    optimized version of scrape

--- casper_scrape.js
+++ casper_scrape.js
@@ -8,35 +8,55 @@ var casper = require("casper").create({
     webSecurityEnabled: false
 });
 
- var user = casper.cli.get(0);
- var password = casper.cli.get(1);
+
+var addressUrl = "http://assessment.nnva.gov/PT/search/commonsearch.aspx?mode=address";
+var addresses = [];
+
+// Manually set number of lines on csv
+var repeatTimes = 1182;
+var count = 0;
 
 casper.userAgent("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36");
-casper.start("http://assessment.nnva.gov/PT/search/commonsearch.aspx?mode=address", function() {
+casper.start(addressUrl, function() {
    this.echo("click agree ")
    this.click("#btAgree");
-});
 
-casper.then(function() {
-    // now search for 'phantomjs' by fillin the form again
-    this.echo("filling out address");
-    this.fillSelectors("form", {
-      "input[name='inpNumber']": "7304",
-      "input[name='inpStreet']": "WARWICK"
-    }, false);
+   stream = fs.open('nnData.csv', 'r');
+   line = stream.readLine();
+   i = 0;
+   while(line) {
+     // casper.echo(line);
+     line = stream.readLine();
+     var nextLine = line.split(',')
+     var addrObj = {
+       streetNum: nextLine[0],
+       street: nextLine[1],
+       owner: nextLine[2]
+     };
+     addresses.push(addrObj);
+     i++;
+   }
+}).repeat(repeatTimes, function(){
+  casper.thenOpen(addressUrl).then(function() {
+      // now search for 'phantomjs' by fillin the form again
+      this.echo("filling out address");
+      this.echo(addresses[count]["streetNum"]);
+      this.fillSelectors("form", {
+        "input[name='inpNumber']": addresses[count]["streetNum"].replace(/"/g,""),
+        "input[name='inpStreet']": addresses[count]["street"].replace(/"/g,"")
+      }, false);
 
-    this.click("#btSearch");
-});
-
-
-
-casper.then(function(){
-  // Grab the Parcel number from the Reverse Lookup
-  this.echo('evaluating');
-  this.echo(this.evaluate(function(){return document.querySelector(".DataletHeaderTop").querySelector("td").innerText.slice(7)}))
-});
-
-
-casper.run(function() {
+      this.click("#btSearch");
+      // this.capture('foo.jpg', undefined, {
+      //   format: 'jpg',
+      //   quality: 75
+      // });
+  }).then(function(){
+    // Grab the Parcel number from the Reverse Lookup
+    this.echo('evaluating');
+    this.echo(this.evaluate(function(){return document.querySelector(".DataletHeaderTop").querySelector("td").innerText.slice(7)}))
+  })
+  count++;
+}).run(function() {
   this.exit();
 });
