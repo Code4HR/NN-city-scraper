09458f924c10894f11812ea09176a63cb35ea48f
Stanley Zheng <szheng@thisisgrow.com>
2016-01-11 23:09:17 -0500

    break out of the loop when necessary

--- reformat_csv.py
+++ reformat_csv.py
@@ -9,22 +9,21 @@ with open("parcel-points.json", 'r') as parcel_data:
   parcel_data = json.load(parcel_data)
 
 to_save = []
-with open('NN_city_owned_addresses.csv') as csvfile:
+with open('test.csv') as csvfile:
   reader = csv.DictReader(csvfile)
   for row in reader:
      print(row["street_num"] + " " + row["street"])
-     for parcel in parcel_data["features"]:
+     for parcel in iter(parcel_data["features"]):
         addr = parcel["properties"]
         query = row["street_num"] + " " + row["street"]
         street = "{} {}".format(addr["HouseNo"], addr["Street"])
         if query in street:
-            print("found ", parcel)
-            to_save.append({"street_num": row["street_num"], "street": row["street"], "owner": row["owner"], "parcel_id": addr["REISID"]})
-        else:
-            to_save.append({"street_num": row["street_num"], "street": row["street"], "owner": row["owner"], "parcel_id": "@@@@@"})
+            # print("found ", parcel)
+            item = {"street_num": row["street_num"], "street": row["street"], "owner": row["owner"], "parcel_id": addr["REISID"]}
+            to_save.append(item)
+            continue
 
-
-with open('reformatted.csv', 'w') as csvfile:
+with open('formatted.csv', 'w') as csvfile:
     fieldnames = ["street_num", "street", "owner", "parcel_id"]
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
