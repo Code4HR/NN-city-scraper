9d37db51a318c518658a1f2bc5dfa6b5a9891ba4
Stanley Zheng <szheng@thisisgrow.com>
2016-01-11 23:27:50 -0500

    fixed logic bugs about iterating over

--- reformat_csv.py
+++ reformat_csv.py
@@ -8,20 +8,26 @@ import json
 with open("parcel-points.json", 'r') as parcel_data:
   parcel_data = json.load(parcel_data)
 
+NUM_OF_RECORDS = len(parcel_data["features"]) - 1
+
 to_save = []
 with open('test.csv') as csvfile:
   reader = csv.DictReader(csvfile)
   for row in reader:
      print(row["street_num"] + " " + row["street"])
-     for parcel in iter(parcel_data["features"]):
+     for dx, parcel in enumerate(parcel_data["features"]):
         addr = parcel["properties"]
         query = row["street_num"] + " " + row["street"]
         street = "{} {}".format(addr["HouseNo"], addr["Street"])
         if query in street:
-            # print("found ", parcel)
+            print("found ", parcel)
             item = {"street_num": row["street_num"], "street": row["street"], "owner": row["owner"], "parcel_id": addr["REISID"]}
             to_save.append(item)
-            continue
+            break
+        if (dx == NUM_OF_RECORDS):
+            # SAVE that we couldn't find the record
+            item = {"street_num": row["street_num"], "street": row["street"], "owner": row["owner"], "parcel_id": "@@@@"}
+            to_save.append(item)
 
 with open('formatted.csv', 'w') as csvfile:
     fieldnames = ["street_num", "street", "owner", "parcel_id"]
