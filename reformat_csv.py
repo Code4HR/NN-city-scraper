6a036c4ab76c66d3cb21d54edcbf41e2b7b614fc
Stanley Zheng <szheng@thisisgrow.com>
2016-01-11 23:45:41 -0500

    updates to print to cout and to file

--- reformat_csv.py
+++ reformat_csv.py
@@ -11,23 +11,29 @@ with open("parcel-points.json", 'r') as parcel_data:
 NUM_OF_RECORDS = len(parcel_data["features"]) - 1
 
 to_save = []
-with open('test.csv') as csvfile:
+with open('NN_city_owned_addresses.csv') as csvfile:
   reader = csv.DictReader(csvfile)
   for row in reader:
-     print(row["street_num"] + " " + row["street"])
+     # print(row["street_num"] + " " + row["street"])
      for dx, parcel in enumerate(parcel_data["features"]):
         addr = parcel["properties"]
         query = row["street_num"] + " " + row["street"]
         street = "{} {}".format(addr["HouseNo"], addr["Street"])
         if query in street:
-            print("found ", parcel)
+            # print("found ", parcel)
             item = {"street_num": row["street_num"], "street": row["street"], "owner": row["owner"], "parcel_id": addr["REISID"]}
             to_save.append(item)
+            data_str = "{},{},{},{}".format(row["street_num"], row["street"], row["owner"], addr["REISID"])
+                        print (data_str)
             break
-        if (dx == NUM_OF_RECORDS):
+
+        if ((dx % NUM_OF_RECORDS) == 0 and dx != 0):
             # SAVE that we couldn't find the record
+            # We want to reprint it but with an indicator which one
             item = {"street_num": row["street_num"], "street": row["street"], "owner": row["owner"], "parcel_id": "@@@@"}
             to_save.append(item)
+            data_str = "{},{},{},{}".format(row["street_num"], row["street"], row["owner"], addr["REISID"])
+            print (data_str)
 
 with open('formatted.csv', 'w') as csvfile:
     fieldnames = ["street_num", "street", "owner", "parcel_id"]
