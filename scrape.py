4454835e0e38e986ccd7722b0046f693e761a80f
Stanley Zheng <szheng@thisisgrow.com>
2016-01-12 08:19:25 -0500

    load from CSV adn do operation to the command line

--- scrape.py
+++ scrape.py
@@ -33,6 +33,7 @@ base_url = "http://assessment.nnva.gov/PT/Datalets/PrintDatalet.aspx?pin={}&gsp=
  "deferred_taxes": "",
  "balance_due": "",
  "parcel_id": "",  # our fake foreign keys
+ "active": "",  # theres an attribute about if the parcel is active
  }
 
 
@@ -40,21 +41,30 @@ async def get_body(client, url):
     async with client.get(url) as response:
         return await response.read()
 
+could_not_find = []
+taxes_not_owed = []
 
-def pull_parcel_info(raw_html):
+
+def pull_parcel_info(raw_html, prop=None):
     parcel = {}
     soap = BeautifulSoup(raw_html, "lxml")
     # Get the Parcel Head Information
     dataheaders = soap.find_all("td", class_=re.compile("DataletHeaderTop"))
     if (dataheaders == []):
-        print ("Doesn't Exist Skipping")
+        # print ("Doesn't Exist Skipping")
+        could_not_find.append(prop)
         return None
-    parcel['parcel_id'] = dataheaders[0].getText().split("PARID:")[1:]
+    parcel['parcel_id'] = dataheaders[0].getText().split("PARID:")[1:][0].strip()
     parcel['address'] = dataheaders[1].getText().strip()
 
     # check their tax bills
-    rows = soap.find(id="Summary of Taxes and Fees Due").find_all('tr')
-    print (len(rows))
+    owes_taxes = soap.find(id="Summary of Taxes and Fees Due")
+    if (owes_taxes is None):
+        # print ("No Taxes Owed")
+        taxes_not_owed.append(prop)
+        return None
+    rows = owes_taxes.find_all('tr')
+    # print (len(rows))
     # get row
     for tr in rows:
         # get columns across
@@ -62,20 +72,30 @@ def pull_parcel_info(raw_html):
         row = (tr.find_all('td'))
         colCheck = row[0].getText().strip()
         if (colCheck != "Tax Year" and colCheck != "Total:" and colCheck != ""):
-            print(row[0].getText(), len(row[0].getText()))
+            entry = ','.join(str(v.getText()) for v in row)
+            data = "{},{},{},{}".format(parcel['parcel_id'], parcel['address'], prop["owner"], entry)
+            print(data)
     return parcel
 
 
-# Checks if there is data on file for this parcel number
-def has_data():
-    return False
-
 if __name__ == '__main__':
+
+    city_props = []
+    # Read in Data
+    with open('output.csv') as csvfile:
+      reader = csv.DictReader(csvfile)
+      for row in reader:
+         city_props.append(row)
+
     loop = asyncio.get_event_loop()
     client = aiohttp.ClientSession(loop=loop)
-    for url_offset in range(base_end - base_num):
-        raw_html = loop.run_until_complete(get_body(client, base_url.format(base_num + url_offset)))
-        parcel = pull_parcel_info(raw_html)
-        if parcel:
-            print (parcel)
+    for prop in city_props:
+        url = base_url.format(prop['parcel_id'])
+        raw_html = loop.run_until_complete(get_body(client, url))
+        parcel = pull_parcel_info(raw_html, prop=prop)
+        # if parcel:
+        #     print (parcel)
     client.close()
+    print ("Could not find, {} properties: {}", len(could_not_find), could_not_find)
+    print ("No Owed Taxes on, {} properties: {}", len(taxes_not_owed), taxes_not_owed)
+
